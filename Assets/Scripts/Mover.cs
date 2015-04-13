b7b87e20373a361c7a0ac3df445249505b35917d
johann2357 <johann2357@gmail.com>
2015-04-12 22:43:18 -0500

    adding bullets

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+	
+	void Start ()
+	{
+		rigidbody.velocity = new Vector3(0, 0, -1) * speed;
+	}
+}
