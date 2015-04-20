8f5731706b01bc513d5d293e42bb66a5699779ae
José Manuel <jose.valdivia.romero@ucsp.edu.pe>
2015-04-20 13:32:27 -0500

    UI First Menu

---
+++ Assets/Scripts/VoiceRecognitionUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoiceRecognitionUI : MonoBehaviour {
+
+	public float speed=1.0f;
+	public Transform start;
+	public Transform end;
+	private SpeechManager speechManager;
+	private float startTime;
+	private float journeyLength;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(speechManager == null)
+		{
+			speechManager = SpeechManager.Instance;
+		}
+		
+		if(speechManager != null && speechManager.IsSapiInitialized())
+		{
+			if(speechManager.IsPhraseRecognized())
+			{
+				string sPhraseTag = speechManager.GetPhraseTagRecognized();
+				print (sPhraseTag);
+				if (sPhraseTag=="PLAY")
+				{
+					Application.LoadLevel("Cajitas");
+					Destroy(gameObject);
+				}
+
+			}
+		}
+	
+	
+	}
+}
