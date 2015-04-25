71ad5b48b5ec20a9ed648921b176af1c74da99c8
Jose E. Carrillo <quique0194@gmail.com>
2015-04-25 17:02:11 -0500

    Fixed life and score texts

--- Assets/LifeController.cs
+++ Assets/LifeController.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public class LifeController : MonoBehaviour {
 
-	public GameObject damageText;
 	public Text lifeText;
 	private int life = 100;
 
@@ -14,7 +13,6 @@ public class LifeController : MonoBehaviour {
 
 	public void OnCollisionEnter(Collision collision) {
 		if (collision.collider.gameObject.CompareTag ("Danino")) {
-			SpawnDamage (1, transform.position.x, transform.position.y);
 			life -= 1;
 			lifeText.text = "LIFE: " + life.ToString ();
 		}
@@ -23,11 +21,5 @@ public class LifeController : MonoBehaviour {
 		}
 	}
 
-	void SpawnDamage (float damage, float x, float y) {
-		x = Mathf.Clamp (x, 0.05f, 0.95f);
-		y = Mathf.Clamp (y, 0.05f, 0.9f);
-		GameObject gui = Instantiate (damageText, new Vector3 (x, y, 0.0f), Quaternion.identity) as GameObject;
-		gui.GetComponent<Text> ().text = damage.ToString ();
-	}
 
 }
